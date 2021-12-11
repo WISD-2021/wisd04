2400e2b66769c8b29aeb28578f65c20f439bde06
蔡羽宣 <3a832055@gm.student.ncut.edu.tw>
2021-12-11 19:08:34 +0800

    bb.b2

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+Route::get('/bb/b1',function(){
+    return view('bb.b1');
+});
+Route::get('/bb/b2',function(){
+    return view('bb.b2');
+});
